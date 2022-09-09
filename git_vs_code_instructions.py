aa965d8fd96153c4d2278779f207f9c33e4adbe8
Jay-Gokani <112378055+Jay-Gokani@users.noreply.github.com>
2022-09-09 08:41:59 +0100

    multi_file_push

--- git_vs_code_instructions.py
+++ git_vs_code_instructions.py
@@ -1,4 +1,5 @@
 # On VS Code, go into Explorer and select the file to edit
 # Save the file
-# Go into Source Control and Commit the changes for the file
-# Still in Source Control, 
+# Go into Source Control and Commit the changes for the file - make sure to include a name
+# Still in Source Control, Push changes - include a name
+# Could be easier to do Commit and Push
