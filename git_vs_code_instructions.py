c2000674360329fdf2f1f000e72b25b3b1188d21
Jay-Gokani <112378055+Jay-Gokani@users.noreply.github.com>
2022-09-10 10:31:48 +0100

    1009_10am_push

--- git_vs_code_instructions.py
+++ git_vs_code_instructions.py
@@ -3,3 +3,8 @@
 # Go into Source Control and Commit the changes for the file - make sure to include a name
 # Still in Source Control, Push changes - include a name
 # Could be easier to do Commit and Push
+
+# When creating a new file, save it in the local repo
+# Then upload it on GitHub
+# Then on VS, save the file then initiate a Pull Request within Source Control
+# Stage changes then Commit and Push
