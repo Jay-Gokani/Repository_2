2e7cf58054b924aed7285692b319f39eae2f6451
Jay-Gokani <112378055+Jay-Gokani@users.noreply.github.com>
2022-09-13 16:35:38 +0100

    20220913_push

--- git_vs_code_instructions.py
+++ git_vs_code_instructions.py
@@ -1,10 +1,9 @@
-# On VS Code, go into Explorer and select the file to edit
-# Save the file
-# Go into Source Control and Commit the changes for the file - make sure to include a name
-# Still in Source Control, Push changes - include a name
-# Could be easier to do Commit and Push
-
+# New file:
 # When creating a new file, save it in the local repo
 # Then upload it on GitHub
 # Then on VS, save the file then initiate a Pull Request within Source Control
 # Stage changes then Commit and Push
+
+# Pre-existing file:
+# Save file in VS Code
+# Source Control > Commit & Push (make sure to include name for Push)
