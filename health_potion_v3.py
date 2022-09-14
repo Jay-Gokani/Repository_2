f7636de366f8f59237d6e2b8909613d3310cb73e
Jay-Gokani <112378055+Jay-Gokani@users.noreply.github.com>
2022-09-14 08:56:21 +0100

    20220914_push_2

--- health_potion_v3.py
+++ health_potion_v3.py
@@ -1,25 +1,28 @@
 import random
 
-Health = 50
+while True:
 
-Vial = random.randint(25,50)
-Potion = random.randint(50,75)
-Elixer = random.randint(75,100)
+    health = 50
 
-Selection = input("Your current Health is 50. Would you like to use the Vial, Potion or Elixer?")
+    vial = random.randint(25,50)
+    potion = random.randint(50,75)
+    elixer = random.randint(75,100)
+    
+    selection = input("Your current health is {}. Would you like to use the Vial, Potion or Elixer?: ".format(health)).strip().capitalize()
 
-if Selection == "Vial":
-  New_Health = (Health + Vial)
-  print("Your New Health increased to", New_Health)
+    if selection == "Vial":
+        print("Your New Health increased to {}".format(health + vial))
 
-elif Selection == "Potion":
-  New_Health = (Health + Potion)  
-  print("Your New Health increased to", New_Health)
+    elif selection == "Potion":
+        print("Your New Health increased to {}".format(health + potion))
 
-elif Selection == "Elixer":
-  New_Health = (Health + Elixer) 
-  print("Your New Health increased to", New_Health)
+    elif selection == "Elixer":
+        print("Your New Health increased to {}".format(health + elixer))
 
-else:
-    print("Try again")
-# This programme works besides the else line, as when a random statement is inputted, then a correct input is entered, it just shows the number for the input rather than New_Health
+    else:
+        print("Try again")
+
+    
+# This programme works, but I want to figure out how to loop it
+# This programme now loops but it 
+
