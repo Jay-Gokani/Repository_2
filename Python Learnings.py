cd5d99d7f5267c18d4f8c40148f8e2ec85fca0fe
Jay-Gokani <112378055+Jay-Gokani@users.noreply.github.com>
2022-09-25 16:59:09 +0100

    20220925_push

--- Python Learnings.py
+++
@@ -1,9 +0,0 @@
-animal = "lion"
-print(animal)
-print(animal)
-print("hellloooo")
-print("there")
-print("push_number_2_test")
-print("push_number_3_test")
-print("push_number_3.1_test")
-print("push_number_3.2.1_test")
--- git_vs_code_instructions.py
+++
@@ -1,9 +0,0 @@
-# New file:
-# When creating a new file, save it in the local repo
-# Then upload it on GitHub
-# Then on VS, save the file then initiate a Pull Request within Source Control
-# Stage changes then Commit and Push
-
-# Pre-existing file:
-# Save file in VS Code
-# Source Control > Commit & Push (make sure to include name for Push)
